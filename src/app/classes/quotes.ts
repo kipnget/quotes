78fefc18e9719bde8548027d32e2b08d328c365a
MUTAI <yegoallen@gmail.com>
2019-10-28 09:49:15 +0300

    this commit adds properties to the class quote

--- src/app/classes/quotes.ts
+++ src/app/classes/quotes.ts
@@ -1,2 +1,20 @@
 export class Quotes {
+    showDetails: boolean;
+    likeState = true;
+    dislikeState = true;
+    touchLike = false;
+    touchDislike = false;
+    constructor(
+        public id: number, 
+        public contributorName: string, 
+        public characterName: string, 
+        public quote: string, 
+        public likeNumber: number,
+        public dislikeNumber: number,
+        public likeStatus: boolean,
+        public delButton: boolean,
+        public submitDate: Date
+        ){
+            this.showDetails = false;
+        }
 }
--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+import {
+  Quotes
+} from '../../classes/quotes';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
+  quotes = [
+    
+  ]
 
-  constructor() { }
+  @Input() quote: Quotes;
+  @Output() isToDelete = new EventEmitter < boolean > ();
 
-  ngOnInit() {
+  deleteMe(index) {
+    this.quotes.splice(index, 1);
   }
+
+  /*Function to add new quote*/
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.unshift(quote);
+    console.log(quote);
+    console.log(this.quotes);
+  }
+
+  /*Function to toggle additional details*/
+  showQuoteDetails(index) {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+    console.log(this.quotes[index]);
+  }
+
+  /* Function to Upvote and Downvote*/
+  onLike(index) {
+    this.quotes[index].touchLike = true;
+    if (this.quotes[index].likeState) {
+      this.quotes[index].likeNumber +=  1;
+      this.quotes[index].likeState = false;
+      
+      //User can't dislike and like at the same time, so reset dislike
+      if (!this.quotes[index].dislikeState){
+        this.quotes[index].dislikeNumber -=  1;
+        this.quotes[index].dislikeState = true;
+      }
+    } else {
+      this.quotes[index].likeNumber -=  1;
+      this.quotes[index].likeState = true;
+    }
+  }
+  onDislike(index) {
+    this.quotes[index].touchDislike = true;
+    if (this.quotes[index].dislikeState) {
+      this.quotes[index].dislikeNumber +=  1;
+      this.quotes[index].dislikeState = false;
+      //User can't dislike and like at the same time, so reset like
+      if (!this.quotes[index].likeState){
+        this.quotes[index].likeNumber -=  1;
+        this.quotes[index].likeState = true;
+      }
+    } else {
+      this.quotes[index].dislikeNumber -=  1;
+      this.quotes[index].dislikeState = true;
+    }
+  }
+  mostLikes(index) {
+      const votes = [];
+      this.quotes.forEach(quote => votes.push(quote.likeNumber));
+      if (this.quotes[index].likeNumber === Math.max(...votes)) {
+        return true;
+      }
+  }
+
+  ngOnInit() {}
 
 }
