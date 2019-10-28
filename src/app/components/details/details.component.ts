67de080206d066eb053019fd455c04b68c5487ea
MUTAI <yegoallen@gmail.com>
2019-10-28 10:35:58 +0300

    this commit will add details components

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Quotes } from '../../classes/quotes'
 
 @Component({
   selector: 'app-details',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+
+  @Input() quote: Quotes;
 
   constructor() { }
 
