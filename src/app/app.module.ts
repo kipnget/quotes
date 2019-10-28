6b71943e73a3a4eae53167dc0eddd82932794036
MUTAI <yegoallen@gmail.com>
2019-10-28 08:32:00 +0300

    craeted files for specific tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './components/details/details.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { ColorRedDirective } from './directives/color-red.directive';
+import { HighlighterDirective } from './directives/highlighter.directive';
+import { DatePipe } from './pipes/date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    FooterComponent,
+    FormComponent,
+    HeaderComponent,
+    QuoteComponent,
+    ColorRedDirective,
+    HighlighterDirective,
+    DatePipe
   ],
   imports: [
     BrowserModule,
