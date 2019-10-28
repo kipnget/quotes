718d7881113c858ce6c5f354f6ef4df1502ed6f5
MUTAI <yegoallen@gmail.com>
2019-10-28 11:50:00 +0300

    this commit will modify highlighter directive

--- src/app/directives/highlighter.directive.ts
+++ src/app/directives/highlighter.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
 })
 export class HighlighterDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef ) { }
+  highlighterElement(){
+    this.el.nativeElement.style.border= '2px solid';
+    this.el.nativeElement.style.color='#000';
+    this.el.nativeElement.style.boxshadow='0px 0px 9px 9px rgba(0, 0, 0, 0.3)';
+  }
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.highlighterElement()
+  }
 }
