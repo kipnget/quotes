3facc185495ce6c356fe0c984929792fea276b23
MUTAI <yegoallen@gmail.com>
2019-10-28 09:24:25 +0300

    this commit adds form components

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Quotes } from '../../classes/quotes';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
 
-  constructor() { }
+export class FormComponent implements OnInit {
+  newQuote =new Quotes (0,"","","", 0, 0, false, true, new Date());
+  @Output() addQuote=new EventEmitter<Quotes>();
+    
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote =new Quotes (0,"","","", 0, 0, false, true, new Date());
+  }
 
   ngOnInit() {
   }
