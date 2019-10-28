2e5f453ed899e79d089662ba04c1838d44f27783
MUTAI <yegoallen@gmail.com>
2019-10-28 11:20:08 +0300

    this commit will modify the date pipes

--- src/app/pipes/date.pipe.ts
+++ src/app/pipes/date.pipe.ts
@@ -5,8 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): any {
+      let now:any = new Date();
+      var diff = now.getTime() - value.getTime();
+      diff/=1000;
+      diff/=(60*60)
+      diff/=24;
+      return Math.ceil(diff) - 1;
+
   }
 
 }
