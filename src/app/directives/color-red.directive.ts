6e3e7e33dca1fd19b0972fb9fee5ed5d733c1b64
MUTAI <yegoallen@gmail.com>
2019-10-28 15:43:11 +0300

    this commit fixes header css and html,highlighetr directive

--- src/app/directives/color-red.directive.ts
+++ src/app/directives/color-red.directive.ts
@@ -7,7 +7,7 @@ export class ColorRedDirective {
 
   constructor (private el:ElementRef) { }
   colorElement() {
-    this.el.nativeElement.style.color = "rgb(212, 5, 5)";
+    this.el.nativeElement.style.color = "rgb(249, 150, 41)";
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
--- src/app/directives/highlighter.directive.ts
+++ src/app/directives/highlighter.directive.ts
@@ -7,8 +7,8 @@ export class HighlighterDirective {
 
   constructor(private el:ElementRef ) { }
   highlighterElement(){
-    this.el.nativeElement.style.border= '2px solid';
-    this.el.nativeElement.style.color='#000';
+    this.el.nativeElement.style.border= '6px solid';
+    this.el.nativeElement.style.color='#0000FF';
     this.el.nativeElement.style.boxshadow='0px 0px 9px 9px rgba(0, 0, 0, 0.3)';
   }
 
